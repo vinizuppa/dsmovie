33f1a378a1ef2fe337f9ebc7575618b86275ae34
Vinicius Cordeiro <vcordeiro12@gmail.com>
2022-01-10 15:54:53 -0300

    Form layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -3,18 +3,18 @@ import './styles.css';
 function Navbar() {
     return (
         <header>
-            <nav className="container">
-                <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/vinizuppa">
-                        <div className="dsmovie-contact-container">
-                            <GithubIcon />
-                            <p className="dsmovie-contact-link">/vinizuppa</p>
-                        </div>
-                    </a>
-                </div>
-            </nav>
-        </header>
+        <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/vinizuppa</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
     );
 }
 
