7f37d1115ae245ff7d71df94f502c2413f5a1ab0
Vinicius Cordeiro <vcordeiro12@gmail.com>
2022-01-14 10:06:34 -0300

    First request

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className="container">
             <div className="dsmovie-nav-content">
                 <h1>DSMovie</h1>
-                <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                <a href="https://github.com/vinizuppa" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
                         <p className="dsmovie-contact-link">/vinizuppa</p>
